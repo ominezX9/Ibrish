47216206ee81a38f42025c1690146c0c67ce6cd2
ominezX9 <winstreakdavid@gmail.con>
2024-11-20 22:48:58 +0100

    feat(tailwindconfig): colors and screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors:{
+      primary: '#B87333', //copper
+      secondary: '#708238', //Olive Green
+      cream: '#F5F5DC', //Off-white
+      grey: '#E8E8E8',
+      lightgrey: '#828282',
+      darkgray: '#2F2F2F',
+      transparent: "transparent",
+    },
     extend: {},
   },
   plugins: [],
