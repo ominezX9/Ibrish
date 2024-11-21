9e000e9641222f4b37ca952399fff66367053653
ominezx9 <68435491+ominezX9@users.noreply.github.com>
2024-11-21 19:13:20 +0000

    feat(loader): add loader

---
+++ src/components/shared/loaders.tsx
@@ -0,0 +1,8 @@
+
+export default function LoaderFull() {
+  return (
+    <div className="flex justify-center items-center">
+      <div className="w-8 h-8 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+    </div>
+  )
+}
