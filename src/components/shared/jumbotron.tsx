c88d8451f20468b5f7b05bfb50f75cc3585dce94
ominezx9 <68435491+ominezX9@users.noreply.github.com>
2024-11-22 01:23:45 +0000

    feat(jumbotron): classname prop added to jumbotron

--- src/components/shared/jumbotron.tsx
+++ src/components/shared/jumbotron.tsx
@@ -1,13 +1,14 @@
 
 type JumbotronProps = {
     children: React.ReactNode;
+    className?: string
 }
 export default function Jumbotron({
     children,
-    
+    className,
 }: JumbotronProps) {
     return (
-        <div className="relative bg-lightgrey px-10 py-10 rounded-lg">
+        <div className={`relative bg-lightgrey px-10 py-10 rounded-lg ${className}`}>
             {children}
         </div>
     )
