9c1d6009543b0e6adc12d10111c6917894d172cc
ominezx9 <68435491+ominezX9@users.noreply.github.com>
2024-11-22 07:00:05 +0000

    update(api): products added

--- src/services/api.ts
+++ src/services/api.ts
@@ -9,3 +9,62 @@ export const CountDetails = [
     }
 
 ]
+
+export const Products = [
+    {
+        id: '/',
+        title: 'Luxury Family Home' ,
+        description: 'A stunning contemporary home designed for comfort and elegance.',
+        price: 1300000,
+        image: 'https://via.placeholder.com/280x280',
+    },
+    {
+        id: '/',
+        title: 'Corporate Office Tower' ,
+        description: 'A modern office space designed to inspire creativity and collaboration',
+        price: 1000000,
+        image: 'https://via.placeholder.com/280x280',
+    },
+    {
+        id: '/',
+        title: 'Luxury Family Home' ,
+        description: 'A stunning contemporary home designed for comfort and elegance.',
+        price: 1300000,
+        image: 'https://via.placeholder.com/280x280',
+    },
+    {
+        id: '/',
+        title: 'Corporate Office Tower' ,
+        description: 'A modern office space designed to inspire creativity and collaboration',
+        price: 1000000,
+        image: 'https://via.placeholder.com/280x280',
+    },
+    {
+        id: '/',
+        title: 'Luxury Family Home' ,
+        description: 'A stunning contemporary home designed for comfort and elegance.',
+        price: 1300000,
+        image: 'https://via.placeholder.com/280x280',
+    },
+    {
+        id: '/',
+        title: 'Corporate Office Tower' ,
+        description: 'A modern office space designed to inspire creativity and collaboration',
+        price: 1000000,
+        image: 'https://via.placeholder.com/280x280',
+    },
+    {
+        id: '/',
+        title: 'Luxury Family Home' ,
+        description: 'A stunning contemporary home designed for comfort and elegance.',
+        price: 1300000,
+        image: 'https://via.placeholder.com/280x280',
+    },
+    {
+        id: '/',
+        title: 'Corporate Office Tower' ,
+        description: 'A modern office space designed to inspire creativity and collaboration',
+        price: 1000000,
+        image: 'https://via.placeholder.com/280x280',
+    },
+]
