3a40df75238a03032b03c09efe3e6dc85b40319b
OX99 <winstreakdavid@gmail.com>
2024-11-23 04:37:55 +0100

    update: wahala patch

--- src/components/menu-bar.tsx
+++ src/components/menu-bar.tsx
@@ -7,6 +7,9 @@ import { useState } from "react";
 
 export default function MenuBar() {
   const [isOpen, setIsOpen] = useState(false)
+  const handleIsOpen = () =>{
+    setIsOpen(!isOpen)
+  }
   return (
     <div className="flex flex-row justify-between items-center py-5 sm:px-[50px] px-[20px] w-full">
       <div className="logo sm:w-[150px] w-[100px]">
@@ -14,7 +17,7 @@ export default function MenuBar() {
           <img src={Logo} alt="logo" />
         </Link>
       </div>
-      <Navbar isOpen={isOpen} />
+      <Navbar isOpen={isOpen} setOpenStatus={(status) => setIsOpen(status)} />
       <div className="lg:block md:hidden hidden">
         <Button 
           size="small"
@@ -25,8 +28,8 @@ export default function MenuBar() {
         </Button>
       </div>
       <div 
-        className="lg:hidden md:hidden block"
-        onClick={() => setIsOpen(true)}
+        className="lg:hidden md:hidden block cursor-pointer"
+        onClick={handleIsOpen}
       >
       <svg stroke="currentColor" fill="none" stroke-width="0" viewBox="0 0 15 15" height="40px" width="40px" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M1.5 3C1.22386 3 1 3.22386 1 3.5C1 3.77614 1.22386 4 1.5 4H13.5C13.7761 4 14 3.77614 14 3.5C14 3.22386 13.7761 3 13.5 3H1.5ZM1 7.5C1 7.22386 1.22386 7 1.5 7H13.5C13.7761 7 14 7.22386 14 7.5C14 7.77614 13.7761 8 13.5 8H1.5C1.22386 8 1 7.77614 1 7.5ZM1 11.5C1 11.2239 1.22386 11 1.5 11H13.5C13.7761 11 14 11.2239 14 11.5C14 11.7761 13.7761 12 13.5 12H1.5C1.22386 12 1 11.7761 1 11.5Z" fill="currentColor"></path></svg>
       </div>
--- src/components/shared/nav-bar.tsx
+++ src/components/shared/nav-bar.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import { useEffect, useState} from "react";
 import useDeviceType from "@utils/get-device-type";
 import Logo from "@assets/images/vector/logo.svg";
-export default function Navbar({isOpen} : {isOpen: boolean}) {
+export default function Navbar({isOpen, setOpenStatus} : {isOpen: boolean, setOpenStatus: (status: boolean) => void}) {
   const deviceType = useDeviceType();
   const options = [
     "Home",
@@ -17,6 +17,7 @@ export default function Navbar({isOpen} : {isOpen: boolean}) {
 
   useEffect(()=>{
     setOpen(isOpen)
+    setOpenStatus(isOpen)
   }, [isOpen])
   return (
     <div className={`bg-white transition-all ease-in-out z-[20] ${deviceType === "phone" ? `fixed bottom-0 ${open ? "left-0" : "-left-[320px]"} w-[300px] top-0 ` : ""}`}>
@@ -27,7 +28,10 @@ export default function Navbar({isOpen} : {isOpen: boolean}) {
               <div className="logo w-[120px] p-5">
                 <img src={Logo} alt="logo" />
               </div>
-              <div className="bg-secondary flex justify-center items-center text-white w-[80px]" onClick={() => setOpen(false)}>
+              <div className="bg-secondary flex justify-center items-center text-white w-[80px] cursor-pointer" onClick={() => {
+                  setOpen(false)
+                  setOpenStatus(false)
+                }}>
                 <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="30px" width="30px" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0z"></path><path d="M19 6.41 17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path></svg>
               </div>
             </div>
